ccc72874aae472ee7c979e42d479c50d65bba36d
godwinsodogandji <jeansodogandji@gmail.com>
2024-06-27 16:55:44 +0100

    Commencement

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import AcceuilComponent from "@/components/AcceuilComponent.vue";
+</script>
+
+<template>
+
+  <AcceuilComponent  />
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/AcceuilComponent.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>AcceuilComponent</div>
+
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+</style>
